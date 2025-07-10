f618d249e7cd3642052579dcabb3220e8955deaf
Alok Gupta <mrxguptaaa@gmail.com>
2025-07-10 14:13:33 +0530

    cors errors fixed

--- server/index.js
+++ server/index.js
@@ -2,13 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "https://dob-app.onrender.com",
+            "http://localhost:5173",
+        ],
+    })
+);
+
+
 app.use(express.json());
 
 app.use("/api", require("./routes/route.js"));
