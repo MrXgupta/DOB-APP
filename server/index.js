1cd843cb72ebf685b716c3ea2ae29aac51b9b9e1
Alok Gupta <mrxguptaaa@gmail.com>
2025-07-10 14:51:39 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
     cors({
         origin: [
-            "https://dob-app.onrender.com",
+            "https://dob-app-sigma.vercel.app",
             "http://localhost:5173",
         ],
     })
