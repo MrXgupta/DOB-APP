afbb950541fa7256e7707ec2d4263ab920614ae3
Alok Gupta <mrxguptaaa@gmail.com>
2025-09-27 15:58:46 +0530

    Update birthdayReminderController.js

--- server/controllers/birthdayReminderController.js
+++ server/controllers/birthdayReminderController.js
@@ -11,6 +11,8 @@ const transporter = nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
     },
+    tls: { rejectUnauthorized: false },
+    connectionTimeout: 20000
 });
 
 const generateBirthdayTiles = (employee) => {
